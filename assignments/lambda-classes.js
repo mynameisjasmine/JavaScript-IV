41b28b69a6701a02eb0b4762ff143db17421bc8f
Jasmine <jasminecharles@hotmail.com>
2019-06-13 13:22:15 -0700

    Built out instructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,28 +1,43 @@
 // CODE here for your Lambda Classes
 
+
 class Person {
     constructor(info) {
     this.name = info.name
     this.age = info.age
+    this.location = info.location
     }
 
 
 speak () {
-    return `Hello my name is ${this.name}`;
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
 }
   
 
 
 
-
-
-
-
-
-
-
-
 }
+
+
+class Instructor extends Person {
+  constructor(info){
+  super(info)
+  this.studentname = info.studentname
+  }
+  
+
+
+  demo(subject) {
+    return `Today we are learning about ${subject}`
+  }
+
+  grade(subject) {
+    return `${this.studentname} receives a perfect score on ${subject}`
+  }
+}
+
+
+//class Student extends v
 
 
 
@@ -30,15 +45,29 @@ speak () {
 
 //// OBJECTS
 
-const fred = new Person({
-    name: 'Fredrico',
-    location: 'Mars',
-    age: 22,
-    favLanguage: 'C++',
-    specialty: 'UI',
-    cathPhrase: 'Yolo'
-});
+
+
+const dan = new Instructor({
+      name: 'Dan',
+      age: 'Infinity',
+      location: 'Denver',
+      specialty: 'Relentless Debugger',
+      favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+      catchPhrase: 'If you can do the thing, you can get paid to do the thing!',
+      studentname: 'Sandy'
+  });
+
+ const fred = new Person({
+     name: 'Fredrico',
+     location: 'Mars',
+     age: 22,
+     favLanguage: 'C++',
+     specialty: 'UI',
+     cathPhrase: 'Yolo'
+ });
 
 
 
-console.log(fred.speak); // Hello my name is Fred, I am from Mars.
+console.log(fred.speak()); // Hello my name is Fred, I am from Mars.
+console.log(dan.demo('cats')); // Today we are learning about (subject)
+console.log(dan.grade('arrays')) // Sandy receives a perfect score on (subject
