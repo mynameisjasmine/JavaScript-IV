b67bd5c22861ebe2a0941a12b9959bfe6dc4d8f8
Jasmine <jasminecharles@hotmail.com>
2019-06-13 12:46:05 -0700

    WIP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,44 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(info) {
+    this.name = info.name
+    this.age = info.age
+    }
+
+
+speak () {
+    return `Hello my name is ${this.name}`;
+}
+  
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+//// OBJECTS
+
+const fred = new Person({
+    name: 'Fredrico',
+    location: 'Mars',
+    age: 22,
+    favLanguage: 'C++',
+    specialty: 'UI',
+    cathPhrase: 'Yolo'
+});
+
+
+
+console.log(fred.speak); // Hello my name is Fred, I am from Mars.
